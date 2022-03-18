74ac6e59329a5683e9ed6bdf9137c8c01c9b13cd
mbrt-yeah <registrations@mbrt.io>
2022-03-18 21:56:45 +0100

    Adds possibilities to render inline-formatting tools

--- packages/core-editor/src/editor-toolbar-inline-formatting/editor-toolbar-inline-formatting.ts
+++ packages/core-editor/src/editor-toolbar-inline-formatting/editor-toolbar-inline-formatting.ts
@@ -1,13 +1,15 @@
 import { ContentObjectToolbarClassName } from "@pennacchi/core/dist/statics";
 import { customElement } from "lit/decorators.js";
 import { Editor } from "../editor/editor";
+import { EditorToolbar } from "../editor-toolbar/editor-toolbar";
 import { getObjectProperty } from "@pennacchi/core/dist/utilities/getObjectProperty";
 import { html, TemplateResult } from "lit";
 import { IContentObject } from "@pennacchi/core-content-object/dist/i-content-object";
-import { EditorToolbar } from "../editor-toolbar/editor-toolbar";
 import { IEditorToolbarInlineFormatting } from "./i-editor-toolbar-inline-formatting";
 import { IEditorToolbarInlineFormattingOptions } from "./i-editor-toolbar-inline-formatting-options";
 import { ITool } from "@pennacchi/core/dist/tool/i-tool";
+import { IToolUiChoiceValue } from "@pennacchi/core/dist/tool/i-tool-ui-choice-value";
+import { msg } from "@lit/localize";
 
 declare global {
     interface HTMLElementTagNameMap {
@@ -41,12 +43,68 @@ export class EditorToolbarInlineFormatting extends EditorToolbar implements IEdi
     /* -------------------------------------------------------------------------- */
 
     public override render(): TemplateResult {
-        if (!this.hasContentObjectFocused())
-            return html``;
+        if (!this.hasContentObjectFocused() 
+            || !this.__toolsFinal
+            || this.__toolsFinal.length === 0
+        ) 
+            return undefined;
 
         this.DOMApi.addClass(ContentObjectToolbarClassName);
 
-        return html`<menu><li>Toolbar Inline Formatting</li></menu>`;
+        return html`<menu>
+            ${
+                this.__toolsFinal.map((tool: ITool) =>
+                    html`<li>${this.renderTool(tool)}</li>`
+                )
+            }
+        </menu>`;
+    }
+
+    private renderTool(tool: ITool): TemplateResult {
+        if (tool.ui.type === "button")
+            return this.renderToolAsButton(tool);
+
+        if (tool.ui.type === "dropdown")
+            return this.renderToolAsDropdown(tool);
+
+        const error = new Error("[EditorToolbarInlineFormatting#renderTool] An error occured");
+        error.message = `Cannot render unknown tool type >${tool.ui.type}<.`;
+        throw error;
+    }
+
+    private renderToolAsButton(tool: ITool): TemplateResult {
+        return html`<pnncch-button>
+            ${ this.renderToolIcon(tool) }
+            <span slot="label">${ msg(tool.ui.label) }</span>
+        </pnncch-button>`;
+    }
+
+    private renderToolAsDropdown(tool: ITool): TemplateResult {
+        if (!tool.ui.values || tool.ui.values.length <= 0) {
+            const error = new Error("[EditorToolbarInlineFormatting#renderToolAsDropdown] An error occured");
+            error.message = `No dropdown values defined`;
+            throw error;
+        }
+
+        return html`
+            <select>
+                <option selected disabled>${tool.ui.label}</option>
+                ${tool.ui.values.map((value: IToolUiChoiceValue) => 
+                    html`<option value="${value.label}">${msg(value.label)}</option>`
+                )}
+            </select>
+        `;
+    }
+
+    private renderToolIcon(tool: ITool): TemplateResult {
+        if ( !tool.ui.icon || !tool.ui.icon.id )
+            return html``;
+
+        return html`<pnncch-icon-svg 
+            iconid="${tool.ui.icon.id}"
+            size="s"
+            slot="prefix"
+        ></pnncch-icon-svg>`;
     }
 
 
