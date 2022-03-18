5f8f02f9e275a8271a5c98d94e319593a69a8f54
mbrt-yeah <registrations@mbrt.io>
2022-03-18 11:06:17 +0100

    Adds new placement type and associated map

---
+++ packages/core/src/maps/placement-map.ts
@@ -0,0 +1,16 @@
+import { Placement } from "../types/placement";
+
+export const PlacementMap: { [key in Placement]: Placement } = {
+    "bottom-end": "bottom-end",
+    "bottom-start": "bottom-end",
+    "bottom": "bottom-end",
+    "left-end": "bottom-end",
+    "left-start": "bottom-end",
+    "left": "bottom-end",
+    "right-end": "bottom-end",
+    "right-start": "bottom-end",
+    "right": "bottom-end",
+    "top-end": "bottom-end",
+    "top-start": "bottom-end",
+    "top": "bottom-end"
+};
